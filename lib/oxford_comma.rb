2535de4df1a9843cf26e9fa529cd48265d52ddb6
Karen C <chenyanhuan66@163.com>
2018-07-24 01:43:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  num = array.size
+  laststring = array.pop
+  str = array.join(", ")
+  array_2 = []
+  array_2 << str
+  array_2 << laststring
+  if num > 2
+    return array_2.join(", and ")
+  else
+    return array_2.join(" and ")
+  end
 end
