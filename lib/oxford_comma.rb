3c989064f733dcdfeaae1c1d47b2a0d262bd39c5
Karen C <chenyanhuan66@163.com>
2018-07-24 01:44:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,20 +7,9 @@ def oxford_comma(array)
   array_2 << laststring
   if num > 2
     return array_2.join(", and ")
-  else
-    return array_2.join(" and ")
+  elsif num == 2
+	  return array_2.join(“ and ”)
+  elsif num == 1
+	  return array_2
   end
-  num = array.size
-laststring = array.pop
-str = array.join(“, ”)
-array_2 = []
-array_2 << str
-array_2 << laststring
-if num > 2
-	return array_2.join(“, and ”)
-elsif num == 2
-	return array_2.join(“ and ”)
-elsif num == 1
-	return array_2
-end
 end
